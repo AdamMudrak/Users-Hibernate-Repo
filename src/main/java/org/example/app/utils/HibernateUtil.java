1967a28355fa7b88371d231b88788e8462fbfdd3
AdamMudrak <mudrets2000@gmail.com>
2023-04-17 12:15:21 +0300

    Add hibernate functionality

---
+++ src/main/java/org/example/app/utils/HibernateUtil.java
@@ -0,0 +1,36 @@
+package org.example.app.utils;
+
+
+import org.example.app.entities.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, Constants.DB_DRIVER);
+                settings.put(Environment.URL, Constants.DB_URL);
+                settings.put(Environment.USER, Constants.DB_USER);
+                settings.put(Environment.PASS, Constants.DB_PASS);
+
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+}
